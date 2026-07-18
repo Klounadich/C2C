af694fb104370b7c49e17c3b3c15a51e23d957d3
klounadich <kekers507y@gmail.com>
2026-07-18 20:09:29 +0300

    hotfix

--- Shared/obj/Debug/net10.0/Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net10.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7211029029ec6edb6ff291b336a4ac651eea87e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f679ad7af61b22d1acfdb2c71d6599b83f2b08d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
