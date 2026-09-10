d866e143deb41648613896a388eca80e4e0dec1e
klounadich <kekers507y@gmail.com>
2026-09-10 11:22:38 +0300

    fix change item func

--- Catalog/Handlers/ChangeItemDataHandler.cs
+++ Catalog/Handlers/ChangeItemDataHandler.cs
@@ -26,10 +26,9 @@ public class ChangeItemDataHandler : IRequestHandler<ChangeItemCommand , bool>
             ItemId = request.ItemId,
             ItemName = request.itemName
         };
-        if (request.image != null)
-        {
+        
             var img_link = await _blobService.UploadFile(request.image, request.ItemId);
-        }
+        
             
         var result = await _catalogRepository.UpdateItemDataAsync(new PutItemData
         {
@@ -39,7 +38,7 @@ public class ChangeItemDataHandler : IRequestHandler<ChangeItemCommand , bool>
             category = request.category,
             price = request.price,
             city =  request.city,
-            
+            image = img_link
             
             
         });
